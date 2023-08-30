b8a63730a553bc387f61f0945840df73d915bd55
kannna5296 <kannnahacks5296@gmail.com>
2023-08-31 08:50:18 +0900

    webサーバ立ち上げ

--- app.go
+++ app.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello World")
+}
 
 func main() {
-  fmt.Printf("Hello World\n")
+  fmt.Printf("HTTPサーバ立ち上げ")
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
 }
